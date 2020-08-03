53f3b77a0ca8a7040180d10c4cca4754b2ce3ff5
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-08-03 20:34:57 +0200

    forEach loop added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let cart = [];
 
+let list = document.querySelector('ul');
+
+items.forEach((item, i) => {
+    list.innerHTML += `<li>
+    <div>Name: ${item.name}</div>
+    <div>Price: $${item.price}</div>
+    image src="${item.image}" />
+    <input type="number" placeholder="quantity" onchange='inputChange(${i}, "${item.name}", ${item.price}>
+    <button>Buy Item</button>
+    <li>
+    `
+});
