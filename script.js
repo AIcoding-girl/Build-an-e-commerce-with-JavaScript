22cea4ba6780a77538b4a0fc3e9fc2c353082d2a
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-09-02 11:05:13 +0300

    img display fixed

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ let items = [
     {
         name: 'Brand Earphones',
         price: 30,
-        image: 'img/earphones.jpg'
+        image:'http://lorempixel.com/200/200/technics/1'
     },
     {
         name: 'Brand Laptop',
         price: 330,
-        image: 'img/laptop.jpg'
+        image:'http://lorempixel.com/200/200/technics/2'
     },
     {
         name: 'Brand TV Screen',
         price: 430,
-        image: 'img/screen.jpg'
+        image:'http://lorempixel.com/200/200/technics/3'
     },
     {
         name: 'Brand Tablet',
         price: 230,
-        image: 'img/tablet.jpg'
+        image:'http://lorempixel.com/200/200/technics/4'
     },
 ]
 
@@ -29,9 +29,8 @@ items.forEach((item, i) => {
     list.innerHTML += `<li>
     <div>Name: ${item.name}</div>
     <div>Price: $${item.price}</div>
-    image src="${item.image}" />
-    <input type="number" placeholder="quantity" onchange='inputChange(${i}, "${item.name}", ${item.price}>
+    <image src="${item.image}" />
     <button>Buy Item</button>
-    <li>
+    </li>
     `
 });
